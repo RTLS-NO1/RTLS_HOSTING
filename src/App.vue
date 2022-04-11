054952226a51e94380aedd6c09c4689d1d4968d5
박기현 <keehyun2@gmail.com>
2022-04-11 15:47:14 +0900

    tt

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <!-- 컨텐츠 영역 -->
+    123123
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {}
+  },
+  watch: {},
+  methods: {},
+}
+</script>
---
+++ vue.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+module.exports = {
+  transpileDependencies: ['vuetify'],
+  outputDir: './dist',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.join(__dirname, 'src/'),
+      },
+    },
+  },
+  css: {
+    loaderOptions: {
+      scss: {
+        prependData: `
+          @import "@/assets/style/place.scss";
+        `,
+      },
+    },
+  },
+}
