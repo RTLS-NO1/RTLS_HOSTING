b113e216e82f31e25feb59bd804465d325e36d0c
박기현 <keehyun2@gmail.com>
2022-04-14 16:52:37 +0900

    console.log 제거

--- src/components/LoginRtls.vue
+++ src/components/LoginRtls.vue
@@ -45,11 +45,11 @@ export default {
     // 로그인 ui 설정
     this.uiConfig = {
       callbacks: {
-        signInSuccessWithAuthResult: function (authResult, redirectUrl) {
-          console.log(authResult)
-          console.log('redirectUrl : ' + redirectUrl)
-          return false
-        },
+        // signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+        //   // console.log(authResult)
+        //   return false
+        // },
+        signInSuccessWithAuthResult: () => false,
         signInSuccessUrl: '/',
         uiShown: function () {
           // The widget is rendered.
@@ -82,14 +82,14 @@ export default {
           // console.log(idToken)
         })
 
-        console.log(user)
-        user.providerData.forEach(function (profile) {
-          console.log('Sign-in provider: ' + profile.providerId)
-          console.log('	Provider-specific UID: ' + profile.uid)
-          console.log('	Name: ' + profile.displayName)
-          console.log('	Email: ' + profile.email)
-          console.log('	Photo URL: ' + profile.photoURL)
-        })
+        // console.log(user)
+        // user.providerData.forEach(function (profile) {
+        //   console.log('Sign-in provider: ' + profile.providerId)
+        //   console.log('	Provider-specific UID: ' + profile.uid)
+        //   console.log('	Name: ' + profile.displayName)
+        //   console.log('	Email: ' + profile.email)
+        //   console.log('	Photo URL: ' + profile.photoURL)
+        // })
         // $('#logoutBtn').show()
       } else {
         startUi()
